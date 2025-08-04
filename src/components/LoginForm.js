d464642d0f049f21080b12accc38fe0ff9ae70a7
Uirá Cardoso <uira.guepardo@gmail.com>
2025-08-04 17:31:07 -0300

    A lot

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,22 @@
 
 export default function LoginForm() {
-    const formulario = document.createElement
-    ('form');
+    const formulario = document.createElement('form');
     
-    const email = document.createElement
-    ('input');
+    const email = document.createElement('input');
     email.type = 'email';
+    email.className = '';
     email.placeholder = "Digite seu e-mail";
     formulario.appendChild(email);
+
+    const senha = document.createElement('input');
+    senha.type = 'password';
+    senha.placeholder = 'digite sua senha';
+    formulario.appendChild(senha);
+
+    const btnLog = document.createElement('button');
+    btnLog.type = 'submit';
+    btnLog.textContent = 'Sing in';
+    formulario.appendChild(btnLog);
 
     return formulario;
 }
